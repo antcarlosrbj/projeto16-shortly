3b5af1ffdc4516dfe8b95ae5fc4636f926e4e9ab
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-06-09 11:16:53 -0300

    /signin (POST)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import connection from "./db.js";
 import joi from "joi";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 export async function signupPOST(req, res) {
     try {
@@ -21,7 +23,7 @@ export async function signupPOST(req, res) {
 
         if (validation.error) {
             console.log(`signupPOST/VALIDATION (JOI) - ${validation.error}`);
-            res.sendStatus(422);
+            res.status(422).send(validation.error);
             return;
         }
 
@@ -30,7 +32,7 @@ export async function signupPOST(req, res) {
 
         if (user.password !== user.confirmPassword) {
             console.log(`signupPOST/CONFIRM PASSWORD`);
-            res.sendStatus(422);
+            res.status(422).send("Senhas devem ser iguais");
             return;
         }
         
@@ -64,6 +66,50 @@ export async function signupPOST(req, res) {
 
 export async function signinPOST(req, res) {
     try {
+
+        const user = req.body;
+
+
+        /* VALIDATION (JOI) */
+
+        const userSchema = joi.object({
+            email: joi.string().email().required(),
+            password: joi.string().required()
+        });
+
+        const validation = userSchema.validate(user);
+
+        if (validation.error) {
+            console.log(`signinPOST/VALIDATION (JOI) - ${validation.error}`);
+            res.status(422).send(validation.error);
+            return;
+        }
+        
+
+        /* CHECK IN THE DATABASE */
+        
+        const result = await connection.query('SELECT * FROM users WHERE email = $1', [user.email]);
+        
+        if(!result.rows[0]) {
+            console.log(`signinPOST/CHECK IN THE DATABASE`);
+            res.sendStatus(401);
+            return;
+        }
+        
+        if (!bcrypt.compareSync(user.password, result.rows[0].password)) {
+            console.log(`signinPOST/CHECK IN THE DATABASE/PASSWORD DOES NOT MATCH`);
+            res.sendStatus(401);
+            return;
+        }
+        
+
+        /* TOKEN SEND */
+
+        const token = jwt.sign({ email: user.email },
+            process.env.JWT_SECRET, { expiresIn: 2592000 }
+        );
+        
+        res.send(token);
 
     } catch (error) {
         console.log(`signinPOST - ${error}`);
