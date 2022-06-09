ab63e380f71e6d947c22384f20e215706babe997
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-06-09 17:19:26 -0300

    /urls/:id (GET)

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -33,10 +33,59 @@ export async function urlsShortenPOST(req, res) {
         res.status(201).send({
             shortUrl: shortUrl
         });
-        
+
 
     } catch (error) {
         console.log(`urlsShortenPOST - ${error}`);
+        res.sendStatus(500);
+    }
+}
+
+
+export async function urlsIdGET(req, res) {
+    try {
+        
+        const data = req.params;
+        data.id = Number(data.id)
+        
+        
+        /* VALIDATION (JOI) */
+
+        const dataSchema = joi.object({
+            id: joi.number().integer().min(1).required()
+        });
+
+        const validation = dataSchema.validate(data);
+
+        if (validation.error) {
+            console.log(`urlsIdGET/VALIDATION (JOI) - ${validation.error}`);
+            res.status(404).send(validation.error);
+            return;
+        }
+        
+
+        /* SEARCH IN THE DATABASE */
+
+        const result = await connection.query('SELECT * FROM urls WHERE id = $1 AND active = true', [data.id]);
+
+        if(!result.rows[0]) {
+            console.log(`urlsIdGET/SEARCH IN THE DATABASE`);
+            res.sendStatus(404);
+            return;
+        }
+
+        
+        /* DATA SUBMISSION */
+
+        res.send({
+            id: result.rows[0].id,
+            shortUrl: result.rows[0].shortUrl,
+            url: result.rows[0].url
+        });
+
+
+    } catch (error) {
+        console.log(`urlsIdGET - ${error}`);
         res.sendStatus(500);
     }
 }
--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -2,11 +2,12 @@ import express from 'express';
 
 
 import {verifyToken} from './../middlewares/authMiddleware.js'
-import {urlsShortenPOST} from './../controllers/urlController.js';
+import {urlsShortenPOST, urlsIdGET} from './../controllers/urlController.js';
 
 
 const urlRouter = express.Router();
 
 urlRouter.post("/urls/shorten", verifyToken, urlsShortenPOST);
+urlRouter.get("/urls/:id", urlsIdGET);
 
 export default urlRouter;
